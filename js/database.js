969fe2658631e57ebf40757d0a40ef21edd08167
[jjsharp] <[jjsharp@usca.edu]>
2023-12-12 16:18:17 -0500

    terminado

--- js/database.js
+++ js/database.js
@@ -1,23 +1,3 @@
-// // add name and scores to the database
-// var usernameInput = "";
-// $("#save").click(saveToLeaderboard());
-// function saveToLeaderboard() {
-//     console.log("Saving to leaderboard");
-//     usernameInput = document.getElementById("nombre").value;
-//     db.collection('playerScores').add({
-//         username: usernameInput,
-//         time: timeTaken,
-//         timestamp: firebase.firestore.FieldValue.serverTimestamp()
-//     })
-//         .then(docRef => {
-//             console.log("Document written with ID: ", docRef.id);
-//         })
-//         .catch(error => {
-//             console.error("Error adding document: ", error);
-//         });
-//     // window.location.href = leaderboard.html;
-// }
-
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
